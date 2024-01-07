346828c1ad03d332964340386e4cdea4fcccd796
DizValheim <122073923+DizValheim@users.noreply.github.com>
2024-01-07 11:19:27 +0530

    Add Dark Mode

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,19 @@
-console.log("Hello Everynyan!");
+$(document).ready(function(){
+  $("#theme-toggle").click( function() {
+    var currentTheme = document.documentElement.getAttribute('data-bs-theme');
+    var newTheme = (currentTheme === 'dark') ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-bs-theme', newTheme);
+
+    // Get the theme-icon element
+    var themeIcon = $("#theme-icon");
+
+    // Check the current class and toggle it
+    if(themeIcon.hasClass("bi-sun")) {
+      themeIcon.removeClass("bi-sun");
+      themeIcon.addClass("bi-moon");
+    } else {
+      themeIcon.removeClass("bi-moon");
+      themeIcon.addClass("bi-sun");
+    }
+  });
+});
