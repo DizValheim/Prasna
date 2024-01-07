4610ec73b3022230dcdcd49a6a91f90f8a64665e
DizValheim <122073923+DizValheim@users.noreply.github.com>
2024-01-07 18:28:46 +0530

    Minor Fixes

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,22 @@
-$(document).ready(function(){
-  $("#theme-toggle").click( function() {
+$(document).ready(function () {
+
+  // var isScrolling = false;
+  // var homeElement = $('#Home');
+  // var topElement = $('#top'); 
+
+
+  // $(window).scroll(function() {
+  //     if(isScrolling) {
+  //         return;
+  //     }
+
+  //     isScrolling = true;
+  //     console.log(isScrolling);
+  //     $('html, body').stop().animate({ scrollTop: homeElement.offset().top }, 100);
+  // });
+
+
+  $("#theme-toggle").click(function () {
     var currentTheme = document.documentElement.getAttribute('data-bs-theme');
     var newTheme = (currentTheme === 'dark') ? 'light' : 'dark';
     document.documentElement.setAttribute('data-bs-theme', newTheme);
@@ -8,7 +25,7 @@ $(document).ready(function(){
     var themeIcon = $("#theme-icon");
 
     // Check the current class and toggle it
-    if(themeIcon.hasClass("bi-sun")) {
+    if (themeIcon.hasClass("bi-sun")) {
       themeIcon.removeClass("bi-sun");
       themeIcon.addClass("bi-moon");
     } else {
